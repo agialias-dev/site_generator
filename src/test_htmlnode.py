0bec77e883e70d7ed636a02292f9306a139b8110
agialias <agialias4000@gmail.com>
2025-05-09 20:28:52 +0100

    Completed up to Chapter 2 Lesson 3.

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -10,7 +10,22 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_props_to_html(self):
         node = HTMLNode("tag", "value", "child", {"class": "prop", "href": "http://example.com"})
+<<<<<<< HEAD
         node.props_to_html()
+=======
+        self.assertEqual(node.props_to_html(), ' class="prop" href="http://example.com"')
+
+    def test_values(self):
+        node = HTMLNode("div", "I wish I could read")
+        self.assertEqual(node.tag, "div")
+        self.assertEqual(node.value, "I wish I could read")
+        self.assertEqual(node.children, None)
+        self.assertEqual(node.props, None)
+    
+    def test_repr(self):
+        node = HTMLNode("p", "What a strange world", None, {"class": "primary"})
+        self.assertEqual(node.__repr__(), "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})")
+>>>>>>> d381000 (Completed up to Chapter 2 Lesson 3.)
 
 if __name__ == "__main__":
     unittest.main()
